17d29ab4ac836dc1a0843c3a366ff88a50807e15
Jeff Jones <jefjones_Macbook@Jeffs-MacBook.local>
2019-02-24 07:14:02 -0800

    update e2e test

--- angular/e2e/src/app.e2e-spec.ts
+++ angular/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to nbc-exercise!');
+    expect(page.getTitleText()).toEqual('Intergalactic Star Wars Database');
   });
 
   afterEach(async () => {
